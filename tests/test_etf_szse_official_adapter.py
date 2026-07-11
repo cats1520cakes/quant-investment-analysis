7cc40ebd6c8bbb0a384c4b6466bfde0a4bf312d0
cats1520cakes <ncgzhhq@gmail.com>
2026-07-12 06:12:15 +0800

    Sync high elasticity approx tests/test_etf_szse_official_adapter.py

--- tests/test_etf_szse_official_adapter.py
+++ tests/test_etf_szse_official_adapter.py
@@ -4,6 +4,7 @@ from quant_proof.free_sources.etf_szse_official_adapter import SzseOfficialDataE
 def test_history_units_and_code():
  p=[{'metadata':{},'error':None,'data':[{'jyrq':'2024-01-02','zqdm':'159915','zqjc':'创业板ETF','qss':'1','ks':'1.1','zg':'1.2','zd':'1.0','ss':'1.15','sdf':'15','cjgs':'2','cjje':'3','syl1':''}]}]
  x=parse_history(p,'159915'); assert x.iloc[0].volume==20000 and x.iloc[0].amount==30000
+ p[0]['data'][0]['cjgs']='10,617.72'; x=parse_history(p,'159915'); assert x.iloc[0].volume==106177200
 def test_history_http_error_fail_closed():
  with pytest.raises(SzseOfficialDataError): parse_history([{'metadata':{},'error':'最多五天','data':[]}],'159915')
 def test_announcement_terminal_pages_and_ambiguity():
